494865c74fe39a3e5b6c2ed51ec31a92dd1d3ba4
Reza Merzaie <ghrezam97@gmail.com>
2023-07-06 03:47:57 +0430

    Fix typo

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,14 +7,14 @@ const Button = ({ onSmash }) => {
   const keys = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
 
   // This function takes the button name as input and calls the updateObjState function
-  const hunldeClick = (btnName) => {
+  const hanldeClick = (btnName) => {
     onSmash(btnName);
   };
 
   return (
     <ul className="keys-container">
       {keys.map((key) => (
-        <li className="key" key={key}><button className="btn" type="button" onClick={() => hunldeClick(key)} key={key}>{key}</button></li>
+        <li className="key" key={key}><button className="btn" type="button" onClick={() => hanldeClick(key)} key={key}>{key}</button></li>
       ))}
     </ul>
   );
