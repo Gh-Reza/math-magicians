af4dca5c8aafd330d66e1b90cd9329ad0197e004
Reza Merzaie <ghrezam97@gmail.com>
2023-07-06 03:45:09 +0430

    Add the event

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,15 +1,32 @@
 import React, { useState } from 'react';
 import './Calculator.css';
-import InputOutput from './InputOutput';
+import Output from './Output';
 import Button from './Button';
+import calculate from '../logic/calculate';
 
 const Calculator = () => {
-  const [inputValue] = useState('0');
+  const [outputValue, setOutpupValue] = useState('0');
+
+  // This object holds the state of the calculator
+  const obj = {
+    total: null,
+    next: null,
+    operation: null,
+  };
+
+  const [objState, setObjState] = useState(obj);
+
+  // This function takes the button name as input and updates the state of the calculator
+  const updateObjState = (btnName) => {
+    const newObj = calculate(objState, btnName);
+    setObjState(newObj);
+    setOutpupValue(newObj.next || newObj.total || '0');
+  };
 
   return (
     <div className="calculator-container">
-      <InputOutput value={inputValue} />
-      <Button />
+      <Output value={outputValue} />
+      <Button onSmash={updateObjState} />
     </div>
   );
 };
