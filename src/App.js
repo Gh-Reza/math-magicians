bd19007eefcec8db16225a5de1dd2b0bf9763d89
Reza Merzaie <ghrezam97@gmail.com>
2023-07-07 04:22:04 +0430

    Integrate api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Calculator from './components/Calculator';
+import Quote from './components/Quote';
 
 function App() {
   return (
     <div className="App">
       <Calculator />
+      <Quote />
     </div>
   );
 }
