0c4005e82942267ea256a841e37850f4bf8a5689
Reza Merzaie <ghrezam97@gmail.com>
2023-07-12 19:50:13 +0430

    Add home page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import Quote from './components/Quote';
 import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
     <>
-      <div className="App">
+      <div className="App d-flex flex-column min-vh-100">
         <Header />
         <Routes>
-          <Route path="/" element={<Calculator className="calc" />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator className="calc" />} />
           <Route path="/quote" element={<Quote className="quote" />} />
         </Routes>
       </div>
